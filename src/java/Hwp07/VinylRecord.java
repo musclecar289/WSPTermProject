396e68347ebae3753eafe68d44df0c547c058770
musclecar289 <musclecar289@gmail.com>
2017-03-22 19:57:22 -0500

    VinylRecord class created

---
+++ src/java/Hwp07/VinylRecord.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hwp07;
+
+/**
+ *
+ * @author Nicholas Clemmons
+ */
+public class VinylRecord {
+    
+    private String albumTitle;
+    private String artistName;
+    private String genre;
+    private int releaseYear;
+    private int numberOfTracks;
+    private int numberOfDiscs;
+    
+    public String getAlbumTitle() {
+        return albumTitle;
+    }
+
+    public void setAlbumTitle(String albumTitle) {
+        this.albumTitle = albumTitle;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getNumberOfTracks() {
+        return numberOfTracks;
+    }
+
+    public void setNumberOfTracks(int numberOfTracks) {
+        this.numberOfTracks = numberOfTracks;
+    }
+
+    public int getNumberOfDiscs() {
+        return numberOfDiscs;
+    }
+
+    public void setNumberOfDiscs(int numberOfDiscs) {
+        this.numberOfDiscs = numberOfDiscs;
+    }
+    
+}
