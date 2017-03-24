e8d6171333b2c7558dd394810b27a5ac7fe7f675
Caleb Dunham <caleb.dunham@yahoo.com>
2017-03-24 13:38:12 -0500

    Nick (#4)
    * VinylRecord class created
    
    * Renamed project folder to TermProject_NicholasC_CalebD_SethH_JuanV and created collection class
    
    * Added CollectionsBean.
    Created new package "JavaObjects" and "databaseScripts".
    Moved and refactored Collection, CollectionsBean and VinylRecord into the new package "JavaObjects".
    Moved Credentials.sql to the new package "databaseScripts".
    
    * Renamed VinylRecord class to Album
    Added member variables to Collection, Album and CollectionsBean.
    Fixed link to adminhome index.html
    
    * Modified Album class.
    Created Album.sql and added a basic ALBUMTABLE.
    Added 3 albums to be inserted.
    Added loadAlbums method to CollectionsBean.
    
    * Added CollectionBean.
    Made collection.xhtml show albums in database

---
+++ src/java/JavaObjects/Album.java
@@ -0,0 +1,82 @@
+package JavaObjects;
+
+/**
+ *
+ * @author Nicholas Clemmons
+ */
+public class Album {
+    
+    private String title;
+    private String artist;
+    private String genre;
+    private int releaseYear;
+    private int numberOfTracks;
+    private int numberOfDiscs;
+    private long albumID;
+    private int albumCount;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getNumberOfTracks() {
+        return numberOfTracks;
+    }
+
+    public void setNumberOfTracks(int numberOfTracks) {
+        this.numberOfTracks = numberOfTracks;
+    }
+
+    public int getNumberOfDiscs() {
+        return numberOfDiscs;
+    }
+
+    public void setNumberOfDiscs(int numberOfDiscs) {
+        this.numberOfDiscs = numberOfDiscs;
+    }
+
+    public long getAlbumID() {
+        return albumID;
+    }
+
+    public void setAlbumID(long albumID) {
+        this.albumID = albumID;
+    }
+
+    public int getAlbumCount() {
+        return albumCount;
+    }
+
+    public void setAlbumCount(int albumCount) {
+        this.albumCount = albumCount;
+    }
+    
+}
---
+++ src/java/JavaObjects/Collection.java
@@ -0,0 +1,17 @@
+
+package JavaObjects;
+
+import java.util.List;
+
+/**
+ *
+ * @author user Nicholas Clemmons
+ */
+
+public class Collection {
+    private String collectionName;
+    private List<Album> records;
+    private int numberOfRecords;
+    private int collectionID;
+    
+}
