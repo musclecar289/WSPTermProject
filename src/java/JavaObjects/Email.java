ba7baa8a76f5eec5227ce9fc4cf0897df678f7b3
juanivelasco <jvelasco2@uco.edu>
2017-04-23 19:01:49 -0500

    Email works clowns

--- src/java/JavaObjects/Email.java
+++ src/java/JavaObjects/Email.java
@@ -1,63 +1,55 @@
 package JavaObjects;
 
-import java.io.Serializable;
-import java.sql.Date;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.*;
 
 @Named(value = "email")
 @RequestScoped
-public class Email implements Serializable {
+public class Email {
 
     public static void send(String regEmail, int num) {
-        System.out.println("Before try");
+       // System.out.println("Before try");
         
         try{
-            System.out.println("IN TRY ");           
+            //System.out.println("IN TRY ");
             
-            String host ="smtp.gmail.com";
+            // variables
+            String host ="smtp.gmail.com" ;
             String user = "Vircoms1@gmail.com";
             String pass = "VircomsOne";
             String to = regEmail;
-            String from = "Vircoms1@gmail.com";
-            String subject = "--Account Verification--";
-            String messageText = "This is confirmation number for your expert programming account. "
-                    + "Please insert this number to activate your account. "
-                    + "Enter the following code on the website: " + num;
+            String from = "Vircoms";
+            String subject = "This is confirmation number for your expertprogramming account. Please insert this number to activate your account.";
+            String messageText = "Enter the following code on the website : " + num;
             boolean sessionDebug = false;
-
+            
+            // Properties
             Properties props = System.getProperties();
-
-            props.put("mail.smtp.starttls.enable", "true");
-            props.put("mail.smtp.host", host);
-            props.put("mail.smtp.port", "587");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.starttls.required", "true");
-
-            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-            
+            props.setProperty("mail.smtp.starttls.enable","true");
+            props.setProperty("mail.smtp.host",host);
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.auth", "false");
+             
             Session mailSession = Session.getDefaultInstance(props, null);
-            mailSession.setDebug(sessionDebug);
+            mailSession.setDebug(sessionDebug); // false
             
+            //msg settings
             Message msg = new MimeMessage(mailSession);
-            msg.setFrom(new InternetAddress(from));
-            
+            msg.setFrom(new InternetAddress(from));            
             InternetAddress[] address = {new InternetAddress(to)};
-            msg.setRecipients(Message.RecipientType.TO, address);
-            msg.setSubject(subject);
-//            msg.setSentDate(new Date());
+            msg.setRecipients(Message.RecipientType.TO, address);           
             msg.setText(messageText);
+            msg.setSubject("Vircoms validation key");
 
-           Transport transport=mailSession.getTransport("smtp");
+            //transfort settings            
+           Transport transport = mailSession.getTransport("smtp");
            transport.connect(host, user, pass);
            transport.sendMessage(msg, msg.getAllRecipients());
            transport.close();
@@ -66,7 +58,11 @@ public class Email implements Serializable {
         {
             System.out.println(ex);
         }
-        System.out.println("End of Send");
+
+    
+
+      // System.out.println("End of Send");
+       
     }
 
     @PostConstruct
